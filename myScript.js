533de956b27d0d4e26cf4f35c48cb0f1779beaab
datalessuk <dataless@gmail.com>
2021-03-20 14:36:55 +0000

    added maps

--- myScript.js
+++ myScript.js
@@ -2,3 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.sidenav');
     let instances = M.Sidenav.init(elems, {});
   });
+  
+  let map;
+
+  function initMap() {
+      //location for main hull area 
+      //marker for center of hull
+  
+      let location = {lat: 53.7664164, lng: -0.10049};
+      let marker = {lat:53.7676,lng:-0.3174};
+      let map = new google.maps.Map(document.getElementById("map"), {
+          center:location,
+          zoom: 9,
+      });
+      new google.maps.Marker({
+          position: marker,
+          map,
+          title:"Here we are",
+      });
+      
+  }
